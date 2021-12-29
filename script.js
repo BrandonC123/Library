1ec12e2aef9039e93ddbac0adba5d5ebb2a60cca
Brandon Chu <brandoc318@gmail.com>
2021-12-29 11:00:59 -0800

    Temporary solution for popup form that displays to screen.

--- script.js
+++ script.js
@@ -7,38 +7,70 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    displayBooks(myLibrary);
+}
 
 function displayBooks(library) {
-    const cardCont = document.querySelector("#card-cont");
-    const cards = document.createElement("div");
-    cards.classList.add("cards");
-    for (let i = 0; i < library.length; i++) {
-        let card = document.createElement("div");
-        card = cardCreater(library[i], card);
-        card.classList.add("card");
-        cards.appendChild(card);
+    if (myLibrary.length >= 1) {
+        const cardCont = document.querySelector("#card-cont");
+        const cards = document.createElement("div");
+        cards.classList.add("cards");
+        for (let i = 0; i < library.length; i++) {
+            let card = document.createElement("div");
+            card = cardCreater(library[i], card);
+            card.classList.add("card");
+            cards.appendChild(card);
+        }
+        cardCont.appendChild(cards);
     }
-    cardCont.appendChild(cards);
 }
 
 function cardCreater(book, card) {
-    for(let prop in book) {
-        const text = document.createElement('div');
+    for (let prop in book) {
+        const text = document.createElement("div");
         text.textContent = book[prop];
-        text.classList.add('card-text');
+        text.classList.add("card-text");
         card.appendChild(text);
     }
     return card;
 }
 
-const b1 = new Book("t", "bar", "123", false);
-myLibrary.push(b1);
+const form = document.querySelector("#form");
 
-const b2 = new Book("t", "bar", "123", false);
-myLibrary.push(b2);
+function openForm() {
+    document.getElementById("form").style.display = "block";
+}
 
-const b3 = new Book("t", "bar", "123", false);
-myLibrary.push(b3);
+function closeForm() {
+    document.getElementById("form").style.display = "none";
+}
+
+const addBtn = document.querySelector("#add");
+addBtn.addEventListener("click", () => {
+    openForm();
+});
+
+const submitBtn = document.querySelector("#submit");
+submitBtn.addEventListener("click", () => {
+    console.log((form.elements[0]).value);
+    console.log((form.elements[1]).value);
+    console.log((form.elements[2]).value);
+    console.log((form.elements[3]).value);
+    const newBook = new Book((form.elements[0]).value, (form.elements[1]).value, 
+    (form.elements[2]).value, (form.elements[3]).value);
+    addBookToLibrary(newBook);
+    closeForm();
+});
+
+// const b1 = new Book("t", "bar", "123", false);
+// myLibrary.push(b1);
+
+// const b2 = new Book("t", "bar", "123", false);
+// myLibrary.push(b2);
+
+// const b3 = new Book("t", "bar", "123", false);
+// myLibrary.push(b3);
 
 displayBooks(myLibrary);
